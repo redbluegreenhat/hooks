76647740605bcdd94424e34572f290abc7fd36a8
Alex <alex@blueselene.com>
2026-02-01 20:47:02 +0100

    another oops

--- src/Exception/HookAlreadyDefinedException.php
+++ src/Exception/HookAlreadyDefinedException.php
@@ -7,4 +7,4 @@ use \BlueSelene\Hooks\Exceptions\HooksBaseException;
 /**
  * Thrown when an attempt is made to define a hook twice
  */
-class HooksBaseException extends HooksBaseException {}
+class HookAlreadyDefinedException extends HooksBaseException {}
