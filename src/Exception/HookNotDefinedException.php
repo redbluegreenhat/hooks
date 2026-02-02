2e235538a4e75b9ccbc05f18a7737570351307b5
Alex <alex@blueselene.com>
2026-02-02 10:49:22 +0100

    Copyright stuff

--- src/Exception/HookNotDefinedException.php
+++ src/Exception/HookNotDefinedException.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * SPDX-License-Identifier: MPL-2.0
+ * Copyright 2026 Alex <alex@blueselene.com>
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 namespace BlueSelene\Hooks\Exception;
 
